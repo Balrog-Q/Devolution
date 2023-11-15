03f0bef613f8e7de6f479be10e3a2e4e679f120d
Fil555 <fzinetti@bluewin.ch>
2023-11-15 19:08:09 +0100

    Replaced timeline class

--- src/devolution/Game.scala
+++ src/devolution/Game.scala
@@ -8,32 +8,34 @@ package devolution
   * specific adventure game that involves a small trip through a twisted forest. All newly created
   * instances of class `Adventure` are identical to each other. To create other kinds of adventure
   * games, you will need to modify or replace the source code of this class. */
-class Timeline:
+class Game:
 
   /** the name of the game */
   val title = "A Forest Adventure"
-  private val middle      = Area("Forest", "You are somewhere in the forest. There are a lot of trees here.\nBirds are singing.")
+  private val bb = Area("Big Bang")
+  /*private val middle      = Area("Forest", "You are somewhere in the forest. There are a lot of trees here.\nBirds are singing.")
   private val northForest = Area("Forest", "You are somewhere in the forest. A tangle of bushes blocks further passage north.\nBirds are singing.")
   private val southForest = Area("Forest", "The forest just goes on and on.")
   private val clearing    = Area("Forest Clearing", "You are at a small clearing in the middle of forest.\nNearly invisible, twisted paths lead in many directions.")
   private val tangle      = Area("Tangle of Bushes", "You are in a dense tangle of bushes. It's hard to see exactly where you're going.")
   private val home        = Area("Home", "Home sweet home! Now the only thing you need is a working remote control.")
-  private val destination = home
   middle     .setNeighbors(Vector("north" -> northForest, "east" -> tangle, "south" -> southForest, "west" -> clearing   ))
   northForest.setNeighbors(Vector(                        "east" -> tangle, "south" -> middle,      "west" -> clearing   ))
   southForest.setNeighbors(Vector("north" -> middle,      "east" -> tangle, "south" -> southForest, "west" -> clearing   ))
   clearing   .setNeighbors(Vector("north" -> northForest, "east" -> middle, "south" -> southForest, "west" -> northForest))
   tangle     .setNeighbors(Vector("north" -> northForest, "east" -> home,   "south" -> southForest, "west" -> northForest))
   home       .setNeighbors(Vector(                                                                  "west" -> tangle     ))
-  clearing.addItem(Item("battery", "It's a small battery cell. Looks new."))
-  southForest.addItem(Item("remote", "It's the remote control for your TV.\nWhat it was doing in the forest, you have no idea.\nProblem is, there's no battery."))
-  /** The character who is the protagonist of the adventure and whom the real-life player controls. */
-  val player = Player(middle)
-  /** The number of turns that have passed since the start of the game. */
+  clearing.addAbility(Ability("battery", "It's a small battery cell. Looks new."))
+  southForest.addAbility(Ability("remote", "It's the remote control for your TV.\nWhat it was doing in the forest, you have no idea.\nProblem is, there's no battery."))
+  *//** The number of turns that have passed since the start of the game. */
   var turnCount = 0
   /** The maximum number of turns that this adventure game allows before time runs out. */
   val timeLimit = 40
   /** Determines if the adventure is complete, that is, if the player has won. */
+
+  /** The character who is the protagonist of the adventure and whom the real-life player controls. */
+  private val destination = bb
+  val player = Player(bb)
   def isComplete =
     this.player.location == this.destination && this.player.has("battery") && this.player.has("remote")
   /** Determines whether the player has won, lost, or quit, thereby ending the game. */
@@ -58,4 +60,4 @@ class Timeline:
     if outcomeReport.isDefined then
       this.turnCount += 1
     outcomeReport.getOrElse(s"""Unknown command: "$command".""")
-end Timeline
+end Game

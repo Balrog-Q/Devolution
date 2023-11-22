0c9cfaf0111e6a69d400889135a93950fa39442f
Fil555 <fzinetti@bluewin.ch>
2023-11-23 00:45:32 +0100

    Tests on commands. Some are randomly broken
    match-case is broken. It ignores "thought" and "learn" case but work for everything else, but the implementation has no differences...

--- src/devolution/Action.scala
+++ src/devolution/Action.scala
@@ -1,8 +1,8 @@
 package devolution
 
-import devolution.helpers.D
-/** The class `Action` represents actions that a player may take in a text adventure game.
-  * `Action` objects are constructed on the basis of textual commands and are, in effect,
+import devolution.helpers.*
+/** The class Action represents actions that a player may take in a text adventure game.
+  * Action objects are constructed on the basis of textual commands and are, in effect,
   * parsers for such commands. An action object is immutable after creation.
   * @param input  a textual in-game command such as “go east” or “rest” */
 class Action(input: String):
@@ -14,21 +14,46 @@ class Action(input: String):
 
   /** Causes the given player to take the action represented by this object, assuming
     * that the command was understood. Returns a description of what happened as a result
-    * of the action (such as “You go west.”). The description is returned in an `Option`
-    * wrapper; if the command was not recognized, `None` is returned. */
-  def execute(actor: Player) = this.verb match
-    case D.actionNames("go")        => Some(actor.go(this.modifiers))
-    case D.actionNames("devolve")   => Some(actor.devolve())
-    case D.actionNames("evolve")    => Some(actor.evolve())
-    //case "knowledge" => Some(actor.knowledge)
-    case D.actionNames("examine")   => Some(actor.examine(this.modifiers))
-    case D.actionNames("quit")      => Some(actor.quit())
-    case D.actionNames("explore")   => Some(actor.enterTimeline())
-    //case D.actionNames("see")       => Some(actor.interact(this.modifiers))
-    case "$"                        => Some("phase " + actor.phase + " " + actor.location.toString) // FOR DEBUG
-    case "tp"                       => Some(actor.tp(this.modifiers))
-    case other if !this.modifiers.isBlank => Some(actor.interact(this.verb, this.modifiers))
-    case other                      => None
+    * of the action (such as “You go west.”). The description is returned in an Option
+    * wrapper; if the command was not recognized, None is returned. */
+  def execute(actor: Player) =
+    println(D.actionNames("learn")+ " "+ this.verb + " " + (D.actionNames("learn") == this.verb)+ " " + (D.actionNames("thought") ==this.verb))
+    /*this.verb match
+    //doesn't work
+      case D.actionNames("learn") => println("1")
+      case D.actionNames("thought") => println("2")
+      case _ => println("3")
+    this.verb match
+    //doesn't work
+      case D.a("learn") => println("1")
+      case D.a("thought") => println("2")
+      case _ => println("3")
+    //work
+    this.verb match
+      case D.b => println("1")
+      case D.c => println("2")
+      case _ => println("3")*/
+
+
+
+    this.verb match
+      case D.actionNames("go")        => Some(actor.go(this.modifiers))
+      case D.actionNames("examine")   => Some(actor.examine(this.modifiers))
+      //case D.actionNames("eat")       => Some("no")
+      case D.actionNames("devolve")   => Some(actor.devolve())
+      case D.actionNames("evolve")    => Some(actor.evolve())
+      //case "knowledge" => Some(actor.knowledge)
+      case D.actionNames("quit")      => Some(actor.quit())
+      case D.actionNames("explore")   => Some(actor.enterTimeline())
+      //case D.actionNames("see")       => Some(actor.interact(this.modifiers))
+      //BROKEN: RANDOMLY DOESN'T WORK
+      case D.actionNames("learn")     => Some(actor.learn(this.modifiers))
+      //BROKEN: RANDOMLY DOESN'T WORK
+      case D.actionNames("thought")   => Some(actor.contemplate())
+      case "$"                        => Some("phase " + actor.phase + " " + actor.location.toString) // FOR DEBUG
+      case "tp"                       => Some(actor.tp(this.modifiers))
+      case other if !this.modifiers.isBlank => Some(actor.interact(this.verb, this.modifiers))
+      case other                      => None
     //case "drop"      => Some(actor.drop(this.modifiers))
     //case "get"       => Some(actor.get(this.modifiers))
     //case "xyzzy"     => Some("The grue tastes yummy.")
